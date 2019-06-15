29974e796ffffb0c496dc750a4b7a6e24a718f55
Unnikrishnan Menon <menon.uk1998@gmail.com>
2019-06-15 15:11:33 +0530

    Add files via upload

---
+++ ai.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import numpy as np
+import random
+
+from operator import add
+from keras.optimizers import Adam
+from keras.models import Sequential
+from keras.layers.core import Dropout,Dense
+
+class entropy(object):
+    def __init__(self):
+        self.recent_memory=np.array([])
+        self.data=pd.DataFrame()
+        self.memory=[]
+        self.actual=[]
+        self.epsilon=0
+        self.reward=0
+        self.learning_rate=0.0005
+        self.gamma=0.9
+        self.qtarget=1
+        self.qpredict=0
+        self.model = self.nn()
+
+    def nn(self,saved_weights=None):
+        model=Sequential()
+        model.add(Dense(output_dim=120, activation='relu', input_dim=11))
+        model.add(Dropout(0.15))
+        model.add(Dense(output_dim=120, activation='relu'))
+        model.add(Dropout(0.15))
+        model.add(Dense(output_dim=120, activation='relu'))
+        model.add(Dropout(0.15))
+        model.add(Dense(output_dim=3, activation='softmax'))
+        opt = Adam(self.learning_rate)
+        model.compile(loss='mse', optimizer=opt)
+        if saved_weights:
+            model.load_weights(saved_weights)
+        return model
+
+    def current_state(self,env,snake,apple):
+        state = [
+            (snake.del_x == 20 and snake.del_y == 0 and (
+                        (list(map(add,snake.pos[-1], [20, 0])) in snake.pos) or
+                        snake.pos[-1][0] + 20 >= (env.width - 20))) or (
+                        snake.del_x == -20 and snake.del_y == 0 and (
+                            (list(map(add, snake.pos[-1], [-20, 0])) in snake.pos) or
+                            snake.pos[-1][0] - 20 < 20)) or (snake.del_x == 0 and snake.del_y == -20 and (
+                        (list(map(add, snake.pos[-1], [0, -20])) in snake.pos) or
+                        snake.pos[-1][-1] - 20 < 20)) or (snake.del_x == 0 and snake.del_y == 20 and (
+                        (list(map(add, snake.pos[-1], [0, 20])) in snake.pos) or
+                        snake.pos[-1][-1] + 20 >= (env.height - 20))),
+
+            (snake.del_x == 0 and snake.del_y == -20 and (
+                        (list(map(add, snake.pos[-1], [20, 0])) in snake.pos) or
+                        snake.pos[-1][0] + 20 > (env.width - 20))) or (
+                        snake.del_x == 0 and snake.del_y == 20 and ((list(map(add, snake.pos[-1],
+                                                                                      [-20, 0])) in snake.pos) or
+                                                                            snake.pos[-1][0] - 20 < 20)) or (
+                        snake.del_x == -20 and snake.del_y == 0 and ((list(map(
+                    add, snake.pos[-1], [0, -20])) in snake.pos) or snake.pos[-1][-1] - 20 < 20)) or (
+                        snake.del_x == 20 and snake.del_y == 0 and (
+                        (list(map(add, snake.pos[-1], [0, 20])) in snake.pos) or snake.pos[-1][
+                    -1] + 20 >= (env.height - 20))),
+
+            (snake.del_x == 0 and snake.del_y == 20 and (
+                        (list(map(add, snake.pos[-1], [20, 0])) in snake.pos) or
+                        snake.pos[-1][0] + 20 > (env.width - 20))) or (
+                        snake.del_x == 0 and snake.del_y == -20 and ((list(map(
+                    add, snake.pos[-1], [-20, 0])) in snake.pos) or snake.pos[-1][0] - 20 < 20)) or (
+                        snake.del_x == 20 and snake.del_y == 0 and (
+                        (list(map(add, snake.pos[-1], [0, -20])) in snake.pos) or snake.pos[-1][
+                    -1] - 20 < 20)) or (
+                    snake.del_x == -20 and snake.del_y == 0 and (
+                        (list(map(add, snake.pos[-1], [0, 20])) in snake.pos) or
+                        snake.pos[-1][-1] + 20 >= (env.height - 20))),
+
+            snake.del_x==-20,
+            snake.del_x==20,
+            snake.del_y==-20,
+            snake.del_y==20,
+            apple.app_x < snake.x,
+            apple.app_x > snake.x,
+            apple.app_y < snake.y,
+            apple.app_y > snake.y
+        ]
+
+        for i in range(len(state)):
+            if state[i]:
+                state[i]=1
+            else:
+                state[i]=0
+        return np.asarray(state)
+
+    def reward_rules(self,snake,dead):
+        self.reward=0
+        if dead:
+            self.reward=-10
+            return self.reward
+        if snake.consumed:
+            self.reward=10
+        return self.reward
+
+    def new_memory_replay(self,memory):
+        if len(memory)>1000:
+            minibatch=random.sample(memory,1000)
+        else:
+            minibatch=memory
+
+        for state,action,reward,next_state,done in minibatch:
+            target=reward
+            if not done:
+                target=reward+self.gamma*np.amax(self.model.predict(np.array([next_state]))[0])
+            target_f=self.model.predict(np.array([state]))
+            target_f[0][np.argmax(action)]=target
+            self.model.fit(np.array([state]),target_f,epochs=1,verbose=0)
+
+    def remember(self,state,action,reward,next_state,done):
+        self.memory.append((state,action,reward,next_state,done))
+
+    def short_memory_training(self,state,action,reward,next_state,done):
+        target=reward
+        if not done:
+            target = reward + self.gamma * np.amax(self.model.predict(next_state.reshape((1, 11)))[0])
+        target_f = self.model.predict(state.reshape((1, 11)))
+        target_f[0][np.argmax(action)] = target
+        self.model.fit(state.reshape((1, 11)), target_f, epochs=1, verbose=0)
---
+++ snake.py
@@ -0,0 +1,172 @@
+import numpy as np
+import pygame
+import random
+from ai import entropy
+from keras.utils import to_categorical
+
+black=(0,0,0)
+speed=30
+render=True
+
+class Snake_env():
+    def __init__(self,width,height):
+        self.width=width
+        self.height=height
+        self.dead=False
+        pygame.display.set_caption('7enTropy7')
+        self.gameDisplay=pygame.display.set_mode((width,height))
+        self.snake=Snake(self)
+        self.apple=Apple()
+        self.game_score=0
+
+
+class Snake(object):
+    def __init__(self, env):
+        self.pos=[]
+        x=env.width*0.45
+        self.x=x-x%20
+        y=env.height*0.5
+        self.y=y-y%20
+        self.pos.append([self.x,self.y])
+        self.apple=1
+        self.consumed=False
+        self.body_image=pygame.image.load('elements/square.png')
+        self.del_x=20
+        self.del_y=0
+
+    def slither(self,choice,x,y,env,apple,agent):
+        temp=[self.del_x,self.del_y]
+        if self.consumed:
+            self.pos.append([self.x,self.y])
+            self.consumed=False
+            self.apple=self.apple+1
+
+        if np.array_equal(choice,[1,0,0]):
+            temp=self.del_x,self.del_y
+        elif np.array_equal(choice,[0,1,0]) and self.del_y==0:
+            temp=[0,self.del_x]
+        elif np.array_equal(choice, [0, 1, 0]) and self.del_x==0:
+            temp=[-self.del_y,0]
+        elif np.array_equal(choice, [0, 0, 1]) and self.del_y==0:
+            temp=[0,-self.del_x]
+        elif np.array_equal(choice, [0, 0, 1]) and self.del_x==0:
+            temp = [self.del_y,0]
+
+        self.del_x,self.del_y=temp
+        self.x=x+self.del_x
+        self.y=y+self.del_y
+
+        if self.x<20 or self.x>env.width-40 or self.y<20 or self.y>env.height-40 or [self.x,self.y] in self.pos:
+            env.dead=True
+        eat_apple(self,apple,env)
+        self.refresh_pos(self.x,self.y)
+
+    def refresh_pos(self,x,y):
+        if self.pos[-1][0]!=x or self.pos[-1][1]!=y:
+            if self.apple>1:
+                for i in range(0,self.apple-1):
+                    self.pos[i][0],self.pos[i][1]=self.pos[i+1]
+            self.pos[-1][0]=x
+            self.pos[-1][1]=y
+
+    def show_snake(self,x,y,apple,env):
+        self.pos[-1][0]=x
+        self.pos[-1][1]=y
+        if env.dead!=True:
+            for i in range(apple):
+                tx,ty = self.pos[len(self.pos)-i-1]
+                env.gameDisplay.blit(self.body_image,(tx,ty))
+            pygame.display.update()
+        else:
+            pygame.time.wait(200)
+
+
+class Apple(object):
+    def __init__(self):
+        self.app_x=240
+        self.app_y=200
+        self.apple_image=pygame.image.load('elements/apple.png')
+
+    def show_apple(self,x,y,env):
+        env.gameDisplay.blit(self.apple_image,(x,y))
+        pygame.display.update()
+
+    def apple_pos(self,env,snake):
+        rx=random.randint(20,env.width-40)
+        self.app_x=rx-rx%20
+        ry = random.randint(20, env.height - 40)
+        self.app_y=ry-ry%20
+        if [self.app_x,self.app_y] not in snake.pos:
+            return self.app_x,self.app_y
+        else:
+            self.apple_pos(env,snake)
+
+
+def eat_apple(snake,apple,env):
+    if snake.x==apple.app_x and snake.y==apple.app_y:
+        apple.apple_pos(env,snake)
+        snake.consumed=True
+        env.game_score=env.game_score+1
+
+def highscore(game_score,record_score):
+    if game_score>=record_score:
+        return game_score
+    else:
+        return record_score
+
+def display_screen(snake,apple,env):
+    env.gameDisplay.fill(black)
+    snake.show_snake(snake.pos[-1][0],snake.pos[-1][1],snake.apple,env)
+    apple.show_apple(apple.app_x,apple.app_y,env)
+
+def start_game(snake,env,apple,agent):
+    start_state1=agent.current_state(env,snake,apple)
+    action=[1,0,0]
+    snake.slither(action,snake.x,snake.y,env,apple,agent)
+    start_state2=agent.current_state(env,snake,apple)
+    rew=agent.reward_rules(snake,env.dead)
+    agent.remember(start_state1,action,rew,start_state2,env.dead)
+    agent.new_memory_replay(agent.memory)
+
+
+def train_snake():
+    pygame.init()
+    agent=entropy()
+    epoch=0
+    highest_record=0
+    while epoch<1000:
+        env=Snake_env(440,440)
+        serpico=env.snake
+        food=env.apple
+
+        start_game(serpico,env,food,agent)
+        if render:
+            display_screen(serpico,food,env)
+
+        while not env.dead:
+            agent.epsilon=80-epoch
+            old_state=agent.current_state(env,serpico,food)
+            if random.randint(0,200)<agent.epsilon:
+                final_move=to_categorical(random.randint(0,2),num_classes=3)
+            else:
+                prediction = agent.model.predict(old_state.reshape((1, 11)))
+                final_move = to_categorical(np.argmax(prediction[0]), num_classes=3)
+
+            serpico.slither(final_move,serpico.x,serpico.y,env,food,agent)
+            new_state=agent.current_state(env,serpico,food)
+
+            reward=agent.reward_rules(serpico,env.dead)
+            agent.short_memory_training(old_state,final_move,reward,new_state,env.dead)
+            agent.remember(old_state,final_move,reward,new_state,env.dead)
+            highest_record=highscore(env.game_score,highest_record)
+
+            if render:
+                display_screen(serpico,food,env)
+                pygame.time.wait(speed)
+
+        agent.new_memory_replay(agent.memory)
+        epoch+=1
+        print('Game : ',epoch,' ------------ Score : ',env.game_score, ' ------------ Highscore : ',highest_record)
+    agent.model.save_weights('serpico.h5')
+
+train_snake()
